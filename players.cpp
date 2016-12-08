ab17565fbe33df36d9e66905caa6ff6ac92b4920
Abdulwahaab Ahmed <Abdulwahaab710@users.noreply.github.com>
2016-12-08 13:32:50 -0500

    Update players.cpp

--- players.cpp
+++ players.cpp
@@ -5,29 +5,32 @@
 //  Created by Ashish Khiani (8385008) and Abdulwahaab Ahmed (8260268)
 //
 
-#include "players.h" 
+#include "players.h"
+#include <iostream>
 
 Player::Player(std::istream& in, CardFactory*){
-   in >> d_name;
+    in >> d_name;
 }
 
-Player::Player(std::string& name){
-   d_name = name;
+Player::Player(std::string& name, Hand& hand){
+    d_name = name;
+    d_hand = hand;
 }
 
 //returns the name of the player
 std::string Player::getName(){
-   return d_name;
+    return d_name;
 }
 
 //returns the number of coins held by the player
 int Player::getCoins(){
-   return coins;
+    return coins;
 }
 
 //add a number of coins
 Player& Player::operator+=(int _coins){
-   coins += _coins;
+    coins += _coins;
+    return *this;
 }
 
 //returns the number of chains possessed by the player (either 2 or 3)
@@ -37,37 +40,45 @@ int Player::getMaxNumChains(){
 
 //returns the non-zero chains
 int Player::getNumChains(){
-   return d_currentChain;
+    return d_currentChain;
 }
 
 //returns the chain at position i
 Chain_Base& Player::operator[](int i){
-   return d_chain[i];
+    return d_chain->at(i);
 }
 
-//adds an empty third chain to the player for TWO OR THREE coins !!!DOUBLE CHECK WITH PROF!!!
+//adds an empty third chain to the player for three coins
 //if the player does not have enough coins, a NotEnoughCoins exception should be thrown
 void Player::buyThirdChain(){
-    if (d_chain > 1 && d_chain != 3){
-       if(coins > 2){
-          throw new NotEnoughCoins;
-       } else{
-          d_maxChains++;
-          coins -= 2;
-       }
+    if (d_maxChains > 1 && d_maxChains != 3){
+        if(coins < 3){
+            throw new NotEnoughCoins;
+        } else{
+            d_maxChains++;
+            coins -= 3;
+        }
     }
 }
 
-//if bool = false -> print top card of player's hand
-//if bool = true -> print all of the cards in the player's hand
-void Player::printHand(std::ostream& o, bool b){
-   o << d_name << "\t" << coins << (coins > 2 ) ? "coins\n" : "coin\n";
-   if (b){
-      o << d_chain[0];
-   } else{
-      for (auto& c : d_chain) {
-         o << c;
-      }
-   }
+//if flag = false -> print top card of player's hand
+//if flag = true -> print all of the cards in the player's hand
+void Player::printHand(std::ostream& out, bool flag){
+    
+    if (flag) {
+        d_hand.print(out);
+    }
+    else {
+        d_hand.top()->print(out);
+    }
+    
+    /*
+    
+     */
 }
- 
+
+void Player::operator<<(std::ostream& out){
+    out << d_name << "\t" << coins << ((coins > 2 ) ? "coins\n" : "coin\n");
+    d_hand.print(out);
+}
+
